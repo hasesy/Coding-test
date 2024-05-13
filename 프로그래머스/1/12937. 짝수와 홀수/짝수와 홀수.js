a9daaf0701305d9508b0300ea7b2bae57867384a
Seoyeon Kim <kseoy0607@gmail.com>
2024-05-13 15:22:54 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -3,7 +3,10 @@ function solution(num) {
     if (num % 2 == 0) {
         answer = 'Even';
     } else {
-        answer = 'Odd';
+        answer = 'Odd'
     }
+    
+    
+    
     return answer;
 }
