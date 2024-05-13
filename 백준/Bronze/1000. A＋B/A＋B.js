ef0f3428ad0ea88075a502400e94556138bf73a3
Seoyeon Kim <kseoy0607@gmail.com>
2024-05-13 12:29:36 +0900

    [Bronze V] Title: A+B, Time: 132 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
-const inputData = fs.readFileSync(0, 'utf8').toString().split(' ');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
 
-const A = parseInt(inputData[0]);
-const B = parseInt(inputData[1]);
+const A = Number(input[0]);
+const B = Number(input[1]);
 
 console.log(A+B);
