3e831a5ddafef00bbb8a5246e311839a01533e1a
Seoyeon Kim <kseoy0607@gmail.com>
2024-07-05 13:42:27 +0900

    [level 2] Title: 올바른 괄호, Time: 21.67 ms, Memory: 55.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Queue<Character> q = new LinkedList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            q.offer(c);
+        }
+
+        int left = 1;
+        int right = 0;
+        char start = q.poll();
+
+        if (start == ')') return false;
+
+        while (!q.isEmpty()) {
+            char par = q.poll();
+            if (par == '(') {
+                left++;
+            } else {
+                right++;
+            }
+
+            if (left < right) {
+                return false;
+            }
+        }
+        
+        if (left == right) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
